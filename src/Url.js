56ac24d335025968103b950672a9ce91ffeed5d7
Bhupesh <93593639+BHUPESH003@users.noreply.github.com>
2024-04-09 22:39:25 +0530

    added backend url for production

--- src/Url.js
+++ src/Url.js
@@ -1,6 +1,7 @@
 export const envKey = {
-    REACT_BASE_URL: "/barnshenn",
-    BASE_URL: "http://localhost:3001/api/v1",
+    REACT_BASE_URL: "/campuscrave",
+    // BASE_URL: "http://localhost:3001/api/v1",
+    BASE_URL: "https://campuscrave-backend.onrender.com/api/v1",
     // BASE_URL: "https://d2a2fyj675.execute-api.ap-south-1.amazonaws.com/dev/",
   
   };
